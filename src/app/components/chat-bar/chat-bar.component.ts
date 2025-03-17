12a15d16e312543d67e3e1558999efbc8972004a
Nino Caduff <ninocaduff@bluewin.ch>
2025-03-17 16:47:24 +0100

    Chat-bar component

---
+++ src/app/components/chat-bar/chat-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-bar',
+  imports: [],
+  templateUrl: './chat-bar.component.html',
+  styleUrl: './chat-bar.component.css'
+})
+export class ChatBarComponent {
+
+}
