eae5fe2bf647021d2c1ef4c6da45fee551e177d3
Nino Caduff <ninocaduff@bluewin.ch>
2025-03-17 17:08:34 +0100

    chat bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
+import { ChatBarComponent } from './components/chat-bar/chat-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ChatBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/chat-bar/chat-bar.component.ts
+++ src/app/components/chat-bar/chat-bar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-bar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat-bar.component.html',
   styleUrl: './chat-bar.component.css'
 })
 export class ChatBarComponent {
+
+chatMessage: string = '';
 
 }
