e339ae85158a5153360b968d45f44513dbef84a6
Nino Caduff <ninocaduff@bluewin.ch>
2025-03-24 23:31:45 +0100

    fixed empty enter

--- src/app/components/chat-bar/chat-bar.component.ts
+++ src/app/components/chat-bar/chat-bar.component.ts
@@ -18,20 +18,25 @@ addMessage(message: string): void {
   const time = now.toLocaleTimeString('de', {
     hour: '2-digit',
     minute: '2-digit'
-  }); // e.g. 22:54
+  });
 
   const messageToSend = `<b>${time}</b> - ${message}<br>`;
   this.submitMessage.emit(messageToSend);
-  this.chatMessage = ''; // clear textarea
 }
 
 
-  onKeyDown(event: KeyboardEvent): void {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault(); // prevent newline
-      this.addMessage(this.chatMessage); // send
-      this.chatMessage = ''; // clear textarea
-    }
+
+onKeyDown(event: KeyboardEvent): void {
+  if (event.key === 'Enter' && !event.shiftKey) {
+    event.preventDefault(); // prevent newline
+
+    const trimmed = this.chatMessage.trim();
+    if (trimmed.length === 0) return; // don't send empty messages
+
+    this.addMessage(trimmed);
+    this.chatMessage = ''; // clear textarea
   }
+}
+
 
 }
